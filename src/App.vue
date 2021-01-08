9f1045cabefd66125509ced6acdb46832dabbece
UNILORN <yuoyun6427.yuniron.komoron@gmail.com>
2021-01-08 23:05:40 +0900

    Homepage initial

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,52 @@
 <template>
   <div id="app">
-    <HelloWorld />
+    <section>  
+      <TopImages />
+    </section>
+
+    <section>
+      <p class="comingsoon">coming soon...</p>
+    </section>
+
+    <section>
+      <p class="copyright">Copyright 2021 unilorn.com</p>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import TopImages from './components/TopImages.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    TopImages
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100&display=swap');
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body,html {
+  font-family: 'Noto Sans JP', sans-serif;
+  margin: 0;
+  background-color: #283593;
+}
+.comingsoon{
+  color:rgba(211, 211, 211, 1);
+  font-size: 20px;
+}
+.copyright {
+  color:rgba(211, 211, 211, 0.9);
+  font-size: 15px;
+  padding-top: 120px;
 }
 </style>
---
+++ src/components/TopImages.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="topimages">
+    <h2>Welcome to</h2>
+    <h1>unilorn.com</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TopImages'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Indie+Flower&display=swap');
+.topimages{
+  height: 700px;
+  background-image: url("/img/top.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+h1,h2 {
+  color: rgba(255,255,255,0.9);
+  font-family: 'Indie Flower', cursive;
+  margin: 0;
+}
+h2 {
+  padding-top: 200px;
+  font-size: 40px;
+}
+h1 {
+  padding-top: 50px;
+  font-size: 60px;
+}
+
+</style>
